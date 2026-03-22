ecfd52682257705e32b8b566ebcac7d99e02f0aa
christian <christian@>
2026-03-22 11:48:32 +0100

    Update example-config

--- nvim/example-config.lua
+++ nvim/example-config.lua
@@ -52,6 +52,8 @@ require("spicy").setup({
       save_dir = vim.fn.expand("~/explanations"),
       side_by_side = false,
       show_line_numbers = true,
+      context_max_chars = 3000,
+      context_surround_lines = 80,
     },
 
     gitmessage = {
